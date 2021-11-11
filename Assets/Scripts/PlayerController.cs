1c015f68d832d9f8a6088247c31ef96743b9f1bf
Austin <aellio23@emich.edu>
2021-11-10 22:35:11 -0500

    Collision detection
    Swap sprite direction based on player movement and added collision detection for walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,13 @@ public class PlayerController : MonoBehaviour
     private Vector3 move;
     private float deltaX;
     private float deltaY;
+    private RaycastHit2D hit;
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     private void Update()
     {
@@ -15,7 +22,27 @@ public class PlayerController : MonoBehaviour
 
         move = new Vector3(deltaX, deltaY, 0);
 
-        transform.Translate(move * Time.deltaTime);
+        // Swap sprite direction; if player is moving right, default sprite orientation. If player moving left, flip the sprite by setting scale.x to -1
+        if(move.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }else if(move.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // We check to see if the player will collide with anything before moving them
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, move.y), Mathf.Abs(move.y * Time.deltaTime), LayerMask.GetMask("Player", "Collidable"));
+        if (hit.collider == null) // BoxCast() returns a RayCastHit2D object; the Collider property of the result will be NULL if nothing was hit, meaning the player can move
+        {
+            transform.Translate(0, move.y * Time.deltaTime, 0);
+        }
+        // We just checked for collisions in the y direction, now we need to check x
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(move.x, 0), Mathf.Abs(move.x * Time.deltaTime), LayerMask.GetMask("Player", "Collidable"));
+        if (hit.collider == null) 
+        {
+            transform.Translate(move.x * Time.deltaTime, 0, 0);
+        }
 
     }
 }
